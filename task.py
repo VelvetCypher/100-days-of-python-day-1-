ec7fd2553785fa31417670f4535965c9144e255f
Bee <user@USERs-MacBook-Air.local>
2026-05-05 22:38:14 +0300

    nickname-generating code

---
+++ task.py
@@ -0,0 +1,11 @@
+print("Welcome to Bee's first python project!")
+
+name = input("What is your name?\n")
+city = input("What city did you grow up in?\n")
+pet = input("what is the name of your pet?\n")
+
+#print(f"Your nickname could be {city}{pet}") ; did this when i couldnt do the string concatenation.
+print(name+ ",Your nickname could be: " +city + " " + pet)
+
+
+
